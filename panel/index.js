aa2bacb410020c18ca1018a0736b52d2228f08f6
Pipi Chen <952157129@qq.com>
2020-05-01 23:39:03 +0800

    update

--- panel/index.js
+++ panel/index.js
@@ -15,24 +15,33 @@ Editor.Panel.extend({
     ui-box-container{
       min-height: 50px;
     }
-
-    .button{
-      float:right
-    }
   `,
 
   template: `
   <div class="container">
-    <h2>构建后自动压缩 PNG</h2>
+    <h2>配置</h2>
     <ui-box-container class="layout vertical left">
       <div>
-        <label>启用 </label><ui-checkbox checked v-value="enabled"></ui-checkbox>
+        <div class="layout horizontal center">
+          <label>启用</label>
+          <ui-checkbox checked v-value="enabled"></ui-checkbox>
+        </div>
         <br>
-        <label>最小图像质量 </label><ui-input v-value="minQuality"></ui-input>
+        <div class="layout horizontal center">
+          <label>最小图像质量</label>
+          <ui-input v-value="minQuality"></ui-input>
+        </div>
         <br>
-        <label>最大图像质量 </label><ui-input v-value="maxQuality"></ui-input>
+        <div class="layout horizontal center">
+          <label>最大图像质量</label>
+          <ui-input v-value="maxQuality"></ui-input>
+        </div>
         <br>
-        <label>质量 </label><ui-slider min=1 max=10 step=1 precision=0 v-value="speed"></ui-slider><label> 速度</label>
+        <div class="layout horizontal center">
+          <label>质量</label>
+          <ui-slider min=1 max=10 step=1 precision=0 v-value="speed"></ui-slider>
+          <label>速度</label>
+        </div>
       </div>
       <br>
       <ui-button class="button blue big" @click="saveConfig()">保存</ui-button>
