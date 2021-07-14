ce01d5025b97f600d8ae159e1e7f7f6f54fc43c2
陈皮皮 <952157129@qq.com>
2021-07-14 11:21:18 +0800

    更新 EditorUtil

--- src/eazax/editor-util.js
+++ src/eazax/editor-util.js
@@ -1,11 +1,52 @@
 const I18n = require('./i18n');
+const PackageUtil = require('./package-util');
 const Updater = require('./updater');
 
 /** 编辑器语言 */
 const LANG = Editor.lang;
 
+/** 包名 */
+const PACKAGE_NAME = PackageUtil.name;
+
 /** 扩展名称 */
 const EXTENSION_NAME = I18n.translate(LANG, 'name');
+
+/**
+ * i18n
+ * @param {string} key 关键词
+ * @returns {string}
+ */
+function translate(key) {
+    return I18n.translate(LANG, key);
+}
+
+/**
+ * 打印信息到控制台
+ * @param {'log' | 'info' | 'warn' | 'error' | string} type 类型 | 内容
+ * @param {any} msgs 消息
+ */
+function print(type, ...msgs) {
+    const message = `[${EXTENSION_NAME}] ${msgs.join(' ')}`;
+    switch (type) {
+        default:
+        case 'log': {
+            Editor.log(message);
+            break;
+        }
+        case 'info': {
+            Editor.info(message);
+            break;
+        }
+        case 'warn': {
+            Editor.warn(message);
+            break;
+        }
+        case 'error': {
+            Editor.error(message);
+            break;
+        }
+    }
+}
 
 /**
  * 编辑器主进程工具 (Cocos Creator 2.x)
@@ -26,52 +67,35 @@ const EditorUtil = {
      * @param {string} key 关键词
      * @returns {string}
      */
-    translate(key) {
-        return I18n.translate(LANG, key);
-    },
+    translate,
 
     /**
      * 打印信息到控制台
      * @param {'log' | 'info' | 'warn' | 'error' | string} type 类型 | 内容
      * @param {any} msgs 消息
      */
-    print(type, ...msgs) {
-        const message = `[${EXTENSION_NAME}] ${msgs.join(' ')}`;
-        switch (type) {
-            default:
-            case 'log': {
-                Editor.log(message);
-                break;
-            }
-            case 'info': {
-                Editor.info(message);
-                break;
-            }
-            case 'warn': {
-                Editor.warn(message);
-                break;
-            }
-            case 'error': {
-                Editor.error(message);
-                break;
-            }
-        }
-    },
+    print,
 
     /**
      * 检查更新
      * @param {boolean} logWhatever 无论有无更新都打印提示
      */
     async checkUpdate(logWhatever) {
+        // 编辑器本次启动是否已经检查过了
+        if (!logWhatever && (Editor[PACKAGE_NAME] && Editor[PACKAGE_NAME].hasCheckUpdate)) {
+            return;
+        }
+        Editor[PACKAGE_NAME] = { hasCheckUpdate: true };
+        // 是否有新版本
         const hasNewVersion = await Updater.check();
         // 打印到控制台
         if (hasNewVersion) {
             const remoteVersion = await Updater.getRemoteVersion();
-            EditorUtil.print('info', `${translate('hasNewVersion')}${remoteVersion}`);
-            EditorUtil.print('info', translate('releases'));
-            EditorUtil.print('info', translate('cocosStore'));
+            print('info', `${translate('hasNewVersion')}${remoteVersion}`);
+            print('info', translate('releases'));
+            print('info', translate('cocosStore'));
         } else if (logWhatever) {
-            EditorUtil.print('info', translate('currentLatest'));
+            print('info', translate('currentLatest'));
         }
     },
 
